982be4fd8b81b5609d5e854e1128a0403fde6fce
Brajesh Singh <brajesh@cosmiccoders.com>
2019-02-13 00:18:37 +0530

    updating tag for the days change

--- buddypress-auto-clean-notifications.php
+++ buddypress-auto-clean-notifications.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: BuddyPress Auto Clean Notifications
- * Version: 1.0.0
+ * Version: 1.0.1
  * Plugin URI: https://buddydev.com/introducing-buddypress-auto-clean-notifications
  * Description: Auto Clean BuddyPress Notifications after a certain period(days).
  * Author: BuddyDev
